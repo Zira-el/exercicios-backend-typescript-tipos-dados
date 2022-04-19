9224f66ccef90037d93de68ec9f50d668b68716f
Nilton Menezes <nilton.menezes@gafit.com.br>
2022-04-18 21:47:23 -0300

    exercicio 3

---
+++ 03/index.ts
@@ -0,0 +1,20 @@
+const values: number[] = [1, 5, 2]
+
+const multiplication = (values: number[]) => {
+    let multiplier: number = 0;
+    let results: string = ''
+
+    if (!values.length) return ''
+
+    values.forEach((value: number) => {
+        while (multiplier <= 10) {
+            results += `${value} X ${multiplier} = ${value * multiplier} \n`
+            multiplier++
+        }
+        multiplier = 0
+    })
+
+    return results
+}
+
+console.log(multiplication(values))
